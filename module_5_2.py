7f4684b4ea73925766731a961589472c5e03046f
PavelKonovalovlessons <konovnovv@mail.ru>
2024-09-28 14:23:41 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,30 @@
+from math import floor
+
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __str__(self):
+        return F"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+print(len(h1))
+print(len(h2))
+print(h1)
+print(h2)
